ab2c8fc08b52841533642376498a38163d099407
martigomezprunera <martigomezprunera@G4-SUBSTITUCIO>
2017-05-02 09:41:47 +0200

    Explicando metodos

--- Jugador.h
+++ Jugador.h
@@ -16,14 +16,14 @@ public:
 	Jugador();										//Constructor
 	~Jugador();										//Destructor
 
-	void setScore(int);								
-	int getScore();			
-	void add(int);
-	void addBasics();								
-	void Delete(int);							
+	void setScore(int);								//Metodo para establecer los puntos del jugador
+	int getScore();									//Metodo para recoger los puntos del jugador
+	void add(int);									//Metodo para añadir un elemento especifico
+	void addBasics();								//Metodo para añadir los elementos basicos 
+	void Delete(int);								//Metodo para eliminar un elemento especifico
 	std::vector<std::string> getInv();				//Metodo para consultar el inventario del jugador
 	void modListaJugador(std::string);				//Metodo para modificar la lista de elementos descubiertos del jugador
-	std::vector<std::string> getListaJugador();				
+	std::vector<std::string> getListaJugador();		//Metodo para recoger la lista del jugador
 	std::string getStringElement(int);				//Consultar elemento con su numero
 	void seeElementsInv();							//METODO PARA PRINTAR POR PANTALLA TODOS LOS ELEMENTOS DEL JUGADOR
 };
