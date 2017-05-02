2d9872285cf1928002955b8bbbd54907a9e15885
martigomezprunera <magopru@hotmail.es>
2017-05-02 23:57:44 +0200

    Update DataM.h

--- DataM.h
+++ DataM.h
@@ -6,13 +6,22 @@
 #include <fstream>
 #include <unordered_map>
 
+template<>
+struct  std::hash<std::pair<std::string, std::string>>
+{
+	size_t operator()(const std::pair<std::string, std::string> &p)const {
+		return ((hash<std::string>()(p.first)
+			^ (hash<std::string>()(p.second) << 1)) >> 1);
+	}
+};
+
 class DataM
 {
 private:
 
 public:
 	std::string lineas[395];
-	std::unordered_map<std::string, std::pair<std::string, std::string>> mymap;
+	std::unordered_map<std::pair<std::string, std::string>, std::string> mymap;
 	const int NUMLINES = 395;
 	DataM(std::string URL);
 	~DataM();
